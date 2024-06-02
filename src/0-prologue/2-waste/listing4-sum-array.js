da4a271872e67ca0b7e5544845eb1c0b2a7c750f
shybyte <shybyte@users.noreply.github.com>
2024-06-02 10:38:12 +0200

    sum array

---
+++ src/0-prologue/2-waste/listing4-sum-array.js
@@ -0,0 +1,104 @@
+import { log } from 'console';
+import { performance, createHistogram } from 'perf_hooks';
+
+// lscpu | grep MHz
+const CPU_MAX_GHZ = 3.6
+
+const ARRAY_SIZE = 4096;
+const TRY_COUNT = 100_000;
+
+const EXPECTED_RESULT = 8386560;
+
+
+function generateNumberArray(size) {
+  const result = new Array(size);
+  // const result = new Uint32Array(size);
+  // const result = new Int32Array(size);
+  // const result = new Int16Array(size);
+  // const result = new Uint16Array(size);
+
+  for (let i = 0; i < size; i++) {
+    result[i] = i;
+  }
+
+  return result;
+}
+
+const array = generateNumberArray(ARRAY_SIZE);
+
+
+function sumArray(array) {
+  let result = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    result += array[i];
+  }
+
+  return result;
+}
+
+function sumArrayForOf(array) {
+  let result = 0;
+
+  for (const el of array) {
+    result += el;
+  }
+
+  return result;
+}
+
+function sumArrayForEach(array) {
+  let result = 0;
+
+  array.forEach(el => {
+    result += el;
+  });
+
+  return result;
+}
+
+function sumArrayReduce(array) {
+  return array.reduce((acc, el) => acc + el);
+}
+
+
+function measureSpeed(expectedResult, callback) {
+  const histogram = createHistogram();
+
+  for (let i = 0; i < TRY_COUNT; i++) {
+    const start = performance.now();
+    const result = callback();
+    if(expectedResult != undefined && expectedResult != result) {
+      throw new Error(`Invalid result ${result} instead of expected result ${expectedResult}`)
+    }
+    const duration = performance.now() - start;
+    histogram.record(Math.floor(duration * 1_000_000)); //nano seconds
+  }
+
+  return histogram;
+}
+
+const benchmarkResults = [];
+
+for (const functionToTest of [sumArray, sumArrayReduce, sumArrayForOf, sumArrayForEach]) {
+  const histogram = measureSpeed(EXPECTED_RESULT, () => functionToTest(array));
+
+  const timeNanoSeconds = histogram.min;
+  const cycles = timeNanoSeconds * CPU_MAX_GHZ;
+  const cyclesPerAdd = cycles / ARRAY_SIZE;
+  const addsPerCicle = 1 / cyclesPerAdd;
+
+  const functionToTestName = functionToTest.name;
+  console.log(`FunctionToTest: ${functionToTestName}`) ;
+  console.log(`Time: ${timeNanoSeconds} nanoseconds`);
+  console.log(`Cycles: ${cycles} cycles`);
+  console.log(`Cycles/add: ${cyclesPerAdd}`);
+  console.log(`Adds/cycle: ${addsPerCicle}`);
+  console.log(``);
+
+  benchmarkResults.push({ name: functionToTestName, timeNanoSeconds, cycles, cyclesPerAdd, addsPerCicle });
+
+}
+
+console.table(benchmarkResults);
+
